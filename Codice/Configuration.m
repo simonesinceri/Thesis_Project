016792654fb70f017f07f27eefe4789df3ed342b
simone <simone.sinceri98@gmail.com>
2022-07-26 19:08:49 +0200

    add all files

---
+++ Codice/Configuration.m
@@ -0,0 +1,9 @@
+% Script configurazione per modello simulink
+
+% Ts tempo di campionamento dello scenario reader
+% lo utilizzo per rate tra le porte 
+ Ts = 0.1;
+
+
+ % egoID, ID del veiocolo controllato
+ egoID = 1;
