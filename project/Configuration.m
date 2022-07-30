4745048fb8fdcc1cd20aec35355a06c415b8eb8e
simone <simone.sinceri98@gmail.com>
2022-07-30 18:47:16 +0200

    schema completato stile black box

--- project/Configuration.m
+++ project/Configuration.m
@@ -9,8 +9,23 @@ load("BusActors1.mat")
 load("BusActors1Actors.mat")
 
 Ts = 0.1;
+numEpisodes = 2;
 
 
 
 % egoID, ID del veiocolo controllato
 egoID = 1;
+
+% Stato Iniziale
+% Per adesso, all'iterazione successiva nel for basta che cambio questi
+% valori ed è fatta
+x_0 = 1;
+y_0 = 1;
+
+x_dot_in = 1;
+steerang_in = 0;
+
+% simulo numEpisodes volte modello Simulink
+for i=0:numEpisodes
+
+end
